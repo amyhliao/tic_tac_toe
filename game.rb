18c3d4a6d491cf81eb69c80f8080d9a02a50d3b4
Amy Liao <amyhliao88@gmail.com>
2016-02-15 22:33:45 -0800

    add start method to game

--- game.rb
+++ game.rb
@@ -1,9 +1,9 @@
 class TicTacToe
-  attr_accessor :board, :win_patterns
+  attr_accessor :board, :win_combos
 
   def initialize
     @board = [" "] * 9
-    @win_patterns = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
+    @win_combos = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
   end
 
   def create_board
@@ -14,5 +14,18 @@ class TicTacToe
     puts "_____________"
     puts "| #{@board[6]} | #{@board[7]} | #{@board[8]} |"
     puts "-------------"
+  end
+
+  def start
+    puts "Instructions: Please enter a number between 1 - 9 for the corresponding cells."
+    puts "_____________",
+         "| 1 | 2 | 3 |",
+         "_____________",
+         "| 4 | 5 | 6 |",
+         "_____________",
+         "| 7 | 8 | 9 |",
+         "_____________"
+    start_game
+    show_results
   end
 end
