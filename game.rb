84f5d7ac050661e3c9e08e402d9d4853f676a027
Amy Liao <amyhliao88@gmail.com>
2016-02-15 21:31:07 -0800

    add create board method

--- game.rb
+++ game.rb
@@ -5,4 +5,14 @@ class TicTacToe
     @board = [" "] * 9
     @win_patterns = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
   end
+
+  def create_board
+    puts "_____________"
+    puts "| #{@board[0]} | #{@board[1]} | #{@board[2]} |"
+    puts "_____________"
+    puts "| #{@board[3]} | #{@board[4]} | #{@board[5]} |"
+    puts "_____________"
+    puts "| #{@board[6]} | #{@board[7]} | #{@board[8]} |"
+    puts "-------------"
+  end
 end
