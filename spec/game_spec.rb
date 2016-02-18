126d2887620ba31a4f647aa55d81afec62ba0724
Amy Liao <amyhliao88@gmail.com>
2016-02-17 20:28:15 -0800

    create spec file to contain tests

---
+++ spec/game_spec.rb
@@ -0,0 +1,11 @@
+require_relative "../game"
+
+describe TicTacToe do
+  let(:game) { TicTacToe.new }
+
+  describe '#' do
+    it "should return a " do
+      expect(game.).to eq()
+    end
+  end
+end
