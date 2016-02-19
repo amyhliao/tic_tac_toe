d7e4d1c5b470cead2bdc7747f278b19cbebae847
Amy Liao <amyhliao88@gmail.com>
2016-02-18 23:26:00 -0800

    add rspec test

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -4,8 +4,16 @@ describe TicTacToe do
   let(:game) { TicTacToe.new }
 
   describe '#create_board' do
-    it "should return a blank board" do
-      expect(game.create_board).to eq(true)
+    it "should create three cells in each row" do
+      game.create_board.each do |row|
+        expect(row.size).to eq(3)
+      end
     end
   end
+
+  # describe '#create_board' do
+  #   it "should return a blank board" do
+  #     expect(game.create_board).to eq(true)
+  #   end
+  # end
 end
