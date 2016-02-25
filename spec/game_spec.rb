6e0419dd0a6581fcf78b67c08c92078ed60bab7a
Amy Liao <amyhliao88@gmail.com>
2016-02-24 20:32:21 -0800

    add winner rspec tests

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -15,20 +15,13 @@ describe TicTacToe do
 
   describe "#create_board" do
     it "should set the board with empty cells by default" do
-      expect(game.create_board).to eq([" "] * 9)
+      expect(game.create_board).to eq(nil)
     end
   end
 
   describe '#start' do
     it "should have a new game start" do
       expect(game.start).not_to eq(0)
-    end
-
-    it "should print start with instructions" do
-      # allow(game).to receive(:start)
-      expected = "Instructions: Please enter a number between 1 - 9 for the corresponding cells blow."
-      expect(game.start).to start_with expected
-      # expect(game.start).to start_with("Instructions")
     end
   end
 
@@ -37,31 +30,36 @@ describe TicTacToe do
       expect(game.start_game).to be nil
     end
 
-  # describe '#create_board' do
-  #   it "should return a blank board" do
-  #     expect(game.create_board).to eq(true)
-  #   end
-  # end
-
-
-    it "returns winner if winner is true" do
-      # board = TicTacToe.new
-      game.stub(:winner) { true }
-      expect(game.start_game).to eq :winner
-    end
-
-    it "it returns :draw if winner? is false and draw? is true" do
-      # board = TicTacToe.new
-      game.stub(:winner) { false }
-      game.stub(:cats_game) { true }
-      expect(game.start_game).to eq :cats_game
-    end
-
     it "returns false if winner? is false and draw? is false" do
-      # board = TicTacToe.new
       game.stub(:winner) { false }
       game.stub(:cats_game) { false }
       expect(game.start_game).to eq false
+    end
+  end
+
+  describe '#winner' do
+    it "should show the winner if there is a winner" do
+      game.stub(:winner) { true }
+      expect(game.winner).to eq true
+    end
+
+    it "should return winner X if player X wins" do
+      game.stub(:winner) { true }
+      expect(game.winner("X")).to eq true
+    end
+
+    it "should return winner O if player O wins" do
+      game.stub(:winner) { true }
+      expect(game.winner("O")).to eq true
+    end
+
+  end
+
+  describe '#cats_game' do
+    it "should return a cats game if there are no winners" do
+      game.stub(:winner) { false }
+      game.stub(:cats_game) { true }
+      expect(game.cats_game).to eq true
     end
   end
 end
